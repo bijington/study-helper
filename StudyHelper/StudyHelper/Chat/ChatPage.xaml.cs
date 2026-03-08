5d5c25f2b6b1916f62d4c713b41f348735c19bc4
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2026-03-08 16:15:50 +0000

    Align closer to a chat style app

--- StudyHelper/StudyHelper/Chat/ChatPage.xaml.cs
+++ StudyHelper/StudyHelper/Chat/ChatPage.xaml.cs
@@ -1,23 +1,59 @@
-﻿namespace StudyHelper.Chat;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StudyHelper.Chat;
 
 public partial class ChatPage : ContentPage
 {
     private readonly ChatService _chatService;
+    private readonly ObservableCollection<ChatEntry> _chatEntries = [];
 
     public ChatPage(ChatService chatService)
     {
         InitializeComponent();
         
         _chatService = chatService;
+        
+        ChatHistory.ItemsSource = _chatEntries;
     }
 
     private async void OnSendClicked(object? sender, EventArgs e)
     {
-        var response = await _chatService.SendMessage(UserPrompt.Text);
+        var userPrompt = UserPrompt.Text;
+        _chatEntries.Add(new ChatEntry { Message = UserPrompt.Text, IsUserSent = true });
+        UserPrompt.Text = string.Empty;
+        
+        var responseEntry = new ChatEntry { Message = "...", IsUserSent = false };
+        _chatEntries.Add(responseEntry);
+        
+        var response = await _chatService.SendMessage(userPrompt);
 
-        if (response.IsSuccess)
-        {
-            Response.Text = response.Text;
-        }
+        responseEntry.Message = response.Text;
+    }
+}
+
+public class ChatEntry : INotifyPropertyChanged
+{
+    public required string Message
+    {
+        get;
+        set => SetField(ref field, value);
+    }
+
+    public bool IsUserSent { get; init; }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
